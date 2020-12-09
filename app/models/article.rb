b2f865597ea19668abc3327988100cd617c58385
Delica Lydia <Delydia84@gmail.com>
2020-12-09 16:47:08 +0200

    add paper clip

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,12 +3,8 @@ class Article < ApplicationRecord
     has_many :taggings
     has_many :tags, through: :taggings
 
-    # def tag_list
-    #     self.tags.collect do |tag|
-    #       tag.name
-    #     end.join(", ")
-    # end
-
+    has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
     def tag_list
       tags.join(", ")
     end

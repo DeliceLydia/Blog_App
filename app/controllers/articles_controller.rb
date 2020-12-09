a8c12c0a6ecd1f474dd4dd25e33cfb6c9f014815
Delica Lydia <Delydia84@gmail.com>
2020-12-09 12:02:34 +0200

    Add delete and edit methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,6 +6,9 @@ class ArticlesController < ApplicationController
   
   def show
       @article = Article.find(params[:id])
+      @comment = Comment.new
+      @comment.article_id = @article.id
+
   end
 
   def new
@@ -15,7 +18,25 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     @article.save
+    flash.notice = "Article '#{@article.title}' Saved!"
     redirect_to article_path(@article)
   end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash.notice = "Article '#{@article.title}' Deleted!"
+    redirect_to(articles_path)
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    flash.notice = "Article '#{@article.title}' Updated!"
+    redirect_to article_path(@article)
+  end
 end
